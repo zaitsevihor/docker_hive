6fa2078f4de9007b500bfad403a7212dce067cfa
Ihor <ihor_zaitsev@govitall.com>
2021-09-20 22:59:43 +0300

    Version1.5

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM alpine
+FROM debian
 COPY . .
 WORKDIR /
-RUN apk update && apk add openssh 
+RUN apt update && apt -y install openssh-server && apt -y install vim
+RUN mkdir -p /root/.ssh
+ADD id_rsa /root/.ssh/id_rsa
+RUN chmod 700 /root/.ssh/id_rsa
+RUN touch /root/.ssh/config
+CMD [ "echo "Host $(/sbin/ip route|awk '/default/ { print $3 }')\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config" ]
--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 #sudo docker network create --subnet=172.18.0.0/16 docker_subnet
 n=1
-while [ $n -ne 21 ]
-do
+#while [ $n -ne 21 ]
+#do
 	sudo docker build -t docker_hive -f Dockerfile .
 	#sudo docker run --rm -it docker_hive /bin/ash
 	#sudo docker run -t -d --rm --name docker_hive_${n} docker_hive
-	sudo docker run -t -d --rm --name docker_hive_${n} --net docker_subnet --ip 172.18.1.${n} docker_hive
+	sudo docker run -it --rm --name docker_hive_${n} --net docker_subnet --ip 172.18.1.${n} docker_hive /bin/bash
 	#sudo docker rmi docker_hive
-	n=$((${n}+1))
-done
+	#n=$((${n}+1))
+#done
